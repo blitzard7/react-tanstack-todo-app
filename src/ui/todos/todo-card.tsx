ca3c47c393d9b2acb34099d8811e559e868ee498
samanta_sophia <samanta-07@hotmail.com>
2025-05-20 20:25:16 +0200

    feat: add shadcn/ui dropdown component for todo card

--- src/ui/todos/todo-card.tsx
+++ src/ui/todos/todo-card.tsx
@@ -2,6 +2,12 @@ import { type Todo } from '@/store/todo-store';
 import { Badge } from '../badge';
 import { ClockIcon, PriorityIcon } from '../icons';
 import { useTranslation } from 'react-i18next';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 interface TodoCardProp {
   todo: Todo;
@@ -19,38 +25,70 @@ function TodoCard({ todo, onDelete }: TodoCardProp) {
     <div className="bg-white shadow-md p-5 border border-gray-200 rounded-lg space-y-2 hover:shadow-lg transition-shadow duration-300">
       <div className="flex justify-between items-start">
         <span className="truncate text-lg font-bold">{todo.title}</span>
-        <Badge className={`${colorClasses[todo.priority]}`}>
-          <PriorityIcon priority={todo.priority} />
-          <span className="ml-1 capitalize">{todo.priority}</span>
-        </Badge>
-        <div
-          className="flex items-center cursor-pointer text-red-600"
-          onClick={() => onDelete(todo.id)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4 mr-2"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span>{t('delete')}</span>
+        <div className="flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-gray-500"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+              </svg>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                </svg>
+                <span>Edit</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="text-red-600"
+                onClick={() => onDelete(todo.id)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 mr-2 text-red-600"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>{t('delete')}</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
 
       <p className="text-gray-600 mb-4 line-clamp-2">{todo.description}</p>
 
-      <div className="flex items-center text-sm text-gray-500 space-x-0.5">
-        <ClockIcon />
-        {todo.deadline ? (
-          <span>{new Date(todo.deadline).toISOString().split('T')[0]}</span>
-        ) : null}
+      <div className="flex justify-between items-center">
+        <div className="flex items-center text-sm text-gray-500 space-x-0.5">
+          <ClockIcon />
+          {todo.deadline ? (
+            <span>{new Date(todo.deadline).toISOString().split('T')[0]}</span>
+          ) : null}
+        </div>
+
+        <Badge className={`${colorClasses[todo.priority]}`}>
+          <PriorityIcon priority={todo.priority} />
+          <span className="ml-1 capitalize">{todo.priority}</span>
+        </Badge>
       </div>
     </div>
   );
